cab3d5071e835a7f93435371dfccccc3efdcb550
farrela46 <gpay46@gmail.com>
2024-03-13 21:44:12 +0700

    Add Retrieve Users fe-vue

--- fe-vue-oscar-bookstore/src/views/admin/ManageUsers.vue
+++ fe-vue-oscar-bookstore/src/views/admin/ManageUsers.vue
@@ -1,13 +1,45 @@
 <script>
-import AuthorsTable from "@/views/components/AuthorsTable.vue";
+import axios from "axios";
+import BASE_URL from '@/api/config-api';
 import ArgonPagination from "@/components/ArgonPagination.vue";
 import ArgonPaginationItem from "@/components/ArgonPaginationItem.vue";
+import ArgonButton from "@/components/ArgonButton.vue";
+import moment from 'moment';
 
 export default {
   components: {
     ArgonPagination,
     ArgonPaginationItem,
-    AuthorsTable
+    ArgonButton,
+  },
+  data() {
+    return {
+      users: [],
+      loading: false
+    };
+  },
+  methods: {
+    formatDate(data_date) {
+      return moment.utc(data_date).format('YYYY-MM-DD')
+    },
+    async getAllUser() {
+      this.loading = true;
+      try {
+        const response = await axios.get(`${BASE_URL}/getUser`, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem('access_token')
+          }
+        });
+        this.users = response.data;
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+  mounted() {
+    this.getAllUser();
   },
 };
 </script>
@@ -18,7 +50,77 @@ export default {
       <div class="col-12">
         <div class="row">
           <div class="col-md-12 col-12">
-            <authors-table />
+            <div class="card">
+              <div class="card-header pb-0 d-flex justify-content-between align-items-center mb-2">
+                <h6 class="mb-0">List Users</h6>
+                <argon-button>Add User</argon-button>
+              </div>
+
+              <div class="card-body px-0 pt-0 pb-2">
+                <div v-if="loading" class="divider">
+                  <v-progress-linear indeterminate></v-progress-linear>
+                </div>
+                <div v-else class="table-responsive p-0">
+                  <table class="table align-items-center mb-0">
+                    <thead>
+                      <tr>
+                        <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                          No
+                        </th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                          Author
+                        </th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                          No Telp
+                        </th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                          Role
+                        </th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                          Tanggal Regist
+                        </th>
+                        <th class="text-secondary opacity-7"></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="(user, index) in users" :key="index">
+                        <td>
+                          <div class="px-2 py-1">
+                            <div class="d-flex justify-content-center">
+                              <h6 class="mb-0 text-sm">{{ index + 1 }}</h6>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <div class="d-flex px-2 py-1">
+                            <div class="d-flex flex-column justify-content-center">
+                              <h6 class="mb-0 text-sm">{{ user.name }}</h6>
+                              <p class="text-xs text-secondary mb-0">
+                                {{ user.email }}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <p class="text-xs font-weight-bold mb-0">+62</p>
+                          <p class="text-xs text-secondary mb-0">{{ user.no_telp }}</p>
+                        </td>
+                        <td class="align-middle text-center text-sm">
+                          <span class="badge badge-sm bg-gradient-success">{{ user.role }}</span>
+                        </td>
+                        <td class="align-middle text-center">
+                          <span class="text-secondary text-xs font-weight-bold">{{ formatDate(user.created_at) }}</span>
+                        </td>
+                        <td class="align-middle">
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
+                            data-original-title="Edit user">Edit</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="row mt-2">
