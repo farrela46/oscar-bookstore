106ab3b1c5c82f2f9c916e902cf93369da949a1b
farrela46 <gpay46@gmail.com>
2024-07-21 02:20:02 +0700

    Feat: mengubah tulisan buat peasnan di cashier

--- fe-vue-oscar-bookstore/src/views/admin/Cashier.vue
+++ fe-vue-oscar-bookstore/src/views/admin/Cashier.vue
@@ -581,7 +581,7 @@ export default {
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="createOfflineOrder">Selesai</v-btn>
+              <v-btn color="blue darken-1" text @click="createOfflineOrder">Buat Pesanan</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
